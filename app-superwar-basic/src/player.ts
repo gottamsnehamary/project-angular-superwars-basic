dc8d45c00528223b000816c89ec96691341631e2
gottamsnehamary <61041030+gottamsnehamary@users.noreply.github.com>
2020-04-02 15:18:36 +0530

    super-wars-basci

---
+++ app-superwar-basic/src/player.ts
@@ -0,0 +1,94 @@
+import { players } from './players';
+
+export const contacts: players[] = [
+  {
+    name: "Spiderman",
+    
+  },
+  ,
+  {
+    name:  "Captain America" ,
+    
+  },
+  {
+    name:"Wonderwoman" ,
+    
+  },
+  {
+    name: "Popcorn",
+    
+  },
+  {
+    name:"Gemwoman", 
+    
+  },
+  {
+    name:"Bolt",
+    
+  },
+  
+  {
+    name:"Antwoman" ,
+    
+  },
+  ,
+  {
+    name:   "Mask" ,
+    
+  },
+  {
+    name:  "Tiger",
+    
+  },
+  {
+    name: "Captain",
+    
+  },
+  {
+    name:"Catwoman", 
+    
+  },
+  {
+    name:"Fish",
+    
+  },
+  
+  {
+    name: "Hulk",
+    
+  },
+  ,
+  {
+    name:  "Ninja" ,
+    
+  },
+  {
+    name: "Black Cat",
+    
+  },
+  {
+    name:  "Volverine",
+    
+  },
+  {
+    name:"Thor", 
+    
+  },
+  {
+    name: "Slayer",
+    
+  },
+  
+  {
+    name: "Vader",
+    
+  },
+  ,
+  {
+    name:  "Slingo" ,
+    
+  },
+ 
+];
+
+
---
+++ app-superwar-basic/src/players.ts
@@ -0,0 +1,5 @@
+export interface players {
+    name: string;
+    
+  }
+  
